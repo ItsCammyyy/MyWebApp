4bb554b538b4a13eb81cc4a2c9f4d3be62660d06
Cameron <camm678@gmail.com>
2019-03-14 17:03:20 +0000

    First test

--- app/src/main/java/com/example/mywebapp/MainActivity.kt
+++ app/src/main/java/com/example/mywebapp/MainActivity.kt
@@ -3,9 +3,14 @@ package com.example.mywebapp
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var listSheep = ArrayList<Sheep>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,6 +18,56 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener {
             startActivity(Intent(this, AddSheepActivity::class.java))
 
+            //Load from DB
+            LoadQuery("%")
+
         }
     }
+
+    private fun LoadQuery(title: String) {
+        var dbManager =  DbManager(this)
+        val projections = arrayOf("ID", "Title", "Description")
+        val selectionArgs = arrayOf(title)
+        val cursor = dbManager.Query(projections, "Title like ?", selectionArgs, "Title")
+        listSheep.clear()
+        if (cursor.moveToFirst()){
+
+            do {
+                val ID = cursor.getInt(cursor.getColumnIndex("ID"))
+                val Title = cursor.getString(cursor.getColumnIndex("Title"))
+                val Description = cursor.getString(cursor.getColumnIndex("Description"))
+
+                listSheep.add(Sheep(ID, Title, Description))
+
+            }while (cursor.moveToNext())
+        }
+        
+        //adapter
+        var mySheepAdapter = MySheepAdapter(this, listSheep)
+    }
+
+    inner class MySheepAdapter : BaseAdapter() {
+        var lsitSheepArray = ArrayList<Sheep>()
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun getItem(position: Int): Any {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun getItemId(position: Int): Long {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+        override fun getCount(): Int {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+    }
+
+
+
+
 }
